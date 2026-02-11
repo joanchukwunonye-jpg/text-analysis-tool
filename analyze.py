5faa34fbd9b82394890525dc66306541e402c644
Joan Chukwunonye <joan.chukwunonye@gmail.com>
2026-02-11 01:36:10 +0100

    Added key sentence extraction

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@ nltk.data.path.append('/home/joan/nltk_data')
 
 from random_username.generate import generate_username
 from nltk.tokenize import word_tokenize, sent_tokenize
+import re
 
 # Welcome User
 def welcomeUser():
@@ -57,6 +58,15 @@ def tokenizeWords(sentences):
         words.extend(word_tokenize(sentence))
     return words
 
+    # Get the key sentences based on search pattern of key words
+def extractKeySentences(sentences, searchPattern):
+	matchedSentences = []
+	for sentence in sentences:
+		# If sentence matches desired pattern, add to matchedSentences
+		if re.search(searchPattern, sentence.lower()):
+			matchedSentences.append(sentence)
+	return matchedSentences
+
 # Get User Details
 welcomeUser()
 username = getUsername()
@@ -67,7 +77,10 @@ articleTextRaw = getArticleText()
 articleSentences = tokenizeSentences(articleTextRaw)
 articleWords = tokenizeWords(articleSentences)
 
+# Get Analytics
+stockSearchPattern = "[0-9]|[%$€£]|thousand|million|billion|trillion|profit|loss"
+keySentences = extractKeySentences(articleSentences, stockSearchPattern)
+
 # Print for testing
 print("GOT:")
-print(articleTextRaw)
-print(articleWords)
+print(keySentences)
